6a7c7e32829d44629ca585348ca2bba26af98a73
[Brandon Teague] <[teaguebrandon@outlook.com]>
2019-10-21 18:48:20 -0400

    looped through and displayed friends

--- pintreach/src/components/Community.js
+++ pintreach/src/components/Community.js
@@ -3,28 +3,58 @@ import { Route, Link } from "react-router-dom";
 import styled from "styled-components";
 import { friends } from "./../data.js";
 
+const sHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-content: flex-start;
+  padding: 0 3% 0 3%;
+  background: grey;
+  color: black;
+  height: 30%;
+`;
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Friends = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Articles = styled.div``;
+
 export const Community = () => {
   //   const [friend, setFriend] = useState([]);
 
-  //   setFriend(friends);
+  //   useEffect(() => {
+  //     setFriend(
+
+  //     );
+  //   }, []);
 
   return (
-    <div>
-      <header>
+    <Body>
+      <sHeader>
         <h2>Pintreach</h2>
         {/* <Link to="/my-board">My Board</Link> */}
-        <Link to="/community">Community</Link>
+        {/* <Link to="/community">Community</Link> */}
         {/* <Link to="/log-in">Log Out</Link> */}
         {/* <Route path="/my-board" component={MyBoard} /> */}
-        <Route path="/community" component={Community} />
+        {/* <Route path="/community" component={Community} /> */}
         {/* <Route path="/log-in" component={LoginPage} /> */}
-      </header>
-      <div className="friendChooser">
-        {/* {friend.map(f => {
-          return <Link>{f.name}</Link>;
-        })} */}
+      </sHeader>
+      <div className="body">
+        <Friends>
+          {friends.map(f => {
+            return <Link>{f.name}</Link>;
+          })}
+        </Friends>
+        <Articles>
+          {friends.map(a => {
+            return <Link>{a.articles}</Link>;
+          })}
+        </Articles>
       </div>
-      <div className="friendDisplay"></div>
-    </div>
+    </Body>
   );
 };
