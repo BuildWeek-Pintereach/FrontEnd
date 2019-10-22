7367a39c0e5ff32817bd3d52dcd272f0a75d14c1
sooshe <sooshe@sooshebot.com>
2019-10-22 11:42:34 -0500

    update myboard for merge

--- pintreach/src/components/MyBoard.js
+++ pintreach/src/components/MyBoard.js
@@ -2,31 +2,14 @@ import React from "react";
 
 
 //this page needs:
-// --A "Welcome" message--Ideally one that says welcome {username}
-// --A List of "My Articles" -- located in reducers file
+//The code in ddArticle is going in here--and the form + action is staying in AddArticle
+// --A "Welcome" message--Ideally one that says welcome {username} (ideal but not required)
+
 // --Add 'Edit Article' and 'Delete Article' Buttons on MyBoard
 // --Link to Community page (do we need edit and delete buttons here???)
 
 function MyBoard(props) {
-  const routeToArticle = (ev, article) => {
-    ev.preventDefault();
-    props.history.push(`/articles-list/${article.id}`);
-  }
-  return (
-    <div className="articles-list-wrapper">
-      {props.articles.map(article => (
-        <div
-          onClick={ev => routeToArticle(ev, article)}
-          className="article-card"
-          key={article.id}
-        >
-          <p>{article.category}</p>
-          <p>{article.title}</p>
-          <p>{article.link}</p>
-        </div>
-      ))}
-    </div>
-  );
+  
 }
 
 export default MyBoard;
